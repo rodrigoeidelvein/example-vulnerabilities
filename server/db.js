a45924056a74df31255ca09f5f9c244313b4c7e9
Rodrigo Fazenda <rodrigoeidelvein@gmail.com>
2021-07-07 23:15:30 -0300

    fix ssl config

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: true
+    ssl: { rejectUnauthorized: false }
 });
 
 module.exports = {
